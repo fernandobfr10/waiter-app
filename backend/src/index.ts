593e3cd4a61cc304d2acbac15cf5547be70970d3
Fernando Junior <fernandobfr10@gmail.com>
2023-10-13 16:01:15 -0300

    config: install and configure connection with mongoose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
-const app = express()
 
-const port = 3001
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express()
+    const port = 3001
 
-app.listen(port, () => {
-  console.log(`🚀 Server is running on http://localhost:${port}`)
-})
+    app.listen(port, () => {
+      console.log(`🚀 Server is running on http://localhost:${port}`)
+    })
+    console.log('📦 Successfully connected to MongoDB')
+  })
+  .catch(() => console.log('❌ Failed to connect to MongoDB'))
+
+
